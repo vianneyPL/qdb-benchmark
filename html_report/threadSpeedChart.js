d3444c37b2f15240369af3c3f10888238c856a25
Benoit Blanchon <github@benoitblanchon.fr>
2015-07-22 17:46:48 +0200

    Add speed in Hz

--- html_report/threadSpeedChart.js
+++ html_report/threadSpeedChart.js
@@ -4,20 +4,32 @@ d3.chart.threadSpeedChart = function() {
 
     var width = 600;
     var height = 600;
-    var leftPadding = 100; 
+    var leftPadding = 80; 
+    var rightPadding = 80; 
     var padding = 30; 
     var data;
     var svg;
 
     function speedText(x)
     {
-        if (x < 1024) return x.toFixed(2) + "B/s";
+        if (x < 1000) return x.toFixed(1) + "Hz";
+        x /= 1000;
+        if (x < 100) return x.toFixed(1) + "kHz";
+        x /= 1000;
+        if (x < 100) return x.toFixed(1) + "MHz";
+        x /= 1000;
+        if (x < 100) return x.toFixed(1) + "GHz";
+    }
+
+    function throughputText(x)
+    {
+        if (x < 1024) return x.toFixed(1) + "B/s";
         x /= 1024;
-        if (x < 1024) return x.toFixed(2) + "kB/s";
+        if (x < 100) return x.toFixed(1) + "kB/s";
         x /= 1024;
-        if (x < 1024) return x.toFixed(2) + "MB/s";
+        if (x < 100) return x.toFixed(1) + "MB/s";
         x /= 1024;
-        if (x < 1024) return x.toFixed(2) + "GB/s";
+        if (x < 100) return x.toFixed(1) + "GB/s";
     }
 
     function chart(container) {
@@ -31,7 +43,8 @@ d3.chart.threadSpeedChart = function() {
 
         svg.append("g").classed("graph", true).attr("transform", "translate("+leftPadding+")");
         svg.append("g").classed("axis", true).classed("x-axis", true).attr("transform", "translate("+leftPadding+","+(height-padding)+")");
-        svg.append("g").classed("axis", true).classed("y-axis", true).attr("transform", "translate("+leftPadding+")");
+        svg.append("g").classed("axis", true).classed("left-axis", true).attr("transform", "translate("+leftPadding+")");
+        svg.append("g").classed("axis", true).classed("right-axis", true).attr("transform", "translate("+(width-rightPadding)+")");
 
         update();
     }
@@ -52,7 +65,7 @@ d3.chart.threadSpeedChart = function() {
                 var iterations = thread.iterations[i] - thread.iterations[i-1];
                 points.push({
                     time: thread.times[i],
-                    speed: data.content_size*iterations*1000.0/elapsed
+                    speed: iterations*1000.0/elapsed
                 });
             }
 
@@ -66,7 +79,7 @@ d3.chart.threadSpeedChart = function() {
         });
         var timeScale = d3.scale.linear()
             .domain([0, timeMax])
-            .range([0, width-leftPadding]);
+            .range([0, width-leftPadding-rightPadding]);
 
         var speedMax = d3.max(threads, function(points) {
             return d3.max(points, function(d) {
@@ -81,7 +94,10 @@ d3.chart.threadSpeedChart = function() {
         var speedScale = d3.scale.linear()
             .domain([speedMin, speedMax])
             .range([height-padding, 0]);
-        console.log("speedMax", speedMax);
+        var throughputScale = d3.scale.linear()
+            .domain([speedMin*data.content_size, speedMax*data.content_size])
+            .range([height-padding, 0]);
+
 
         var lineFunc = d3.svg.line()
             .x(function(d) { return timeScale(d.time); })
@@ -110,8 +126,11 @@ d3.chart.threadSpeedChart = function() {
 
         var speedAxis = d3.svg.axis().scale(speedScale).orient("left")
             .tickFormat(function(d) { return speedText(d); })
-        svg.selectAll(".y-axis").call(speedAxis); 
+        svg.selectAll(".left-axis").call(speedAxis); 
 
+        var throughputAxis = d3.svg.axis().scale(throughputScale).orient("right")
+            .tickFormat(function(d) { return throughputText(d); })
+        svg.selectAll(".right-axis").call(throughputAxis); 
     }
 
     chart.data = function(value) {
