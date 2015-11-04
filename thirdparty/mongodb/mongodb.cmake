a885aef392d35ec13d2e17947e6b21644cc6e94e
Benoit Blanchon <benoit@quasardb.net>
2015-11-04 18:20:22 +0100

    Disable mongodb if boost is not found

--- thirdparty/mongodb/mongodb.cmake
+++ thirdparty/mongodb/mongodb.cmake
@@ -1,3 +1,23 @@
+find_library(BOOST_REGEX  boost_regex)
+find_library(BOOST_SYSTEM boost_system)
+find_library(BOOST_THREAD boost_thread)
+find_path(BOOST_INCLUDE_DIR boost/version.hpp)
+
+if(NOT EXISTS ${BOOST_REGEX})
+    message(STATUS "libboost-regex-dev not found, disabling mongodb")
+    return()
+endif()
+
+if(NOT EXISTS ${BOOST_SYSTEM})
+    message(STATUS "libboost-system-dev not found, disabling mongodb")
+    return()
+endif()
+
+if(NOT EXISTS ${BOOST_THREAD})
+    message(STATUS "libboost-thread-dev not found, disabling mongodb")
+    return()
+endif()
+
 message(STATUS "Looking for mongodb API in ${CMAKE_CURRENT_LIST_DIR}")
 
 find_library(MONGOCLIENT
@@ -8,20 +28,4 @@ find_library(MONGOCLIENT
 find_path(MONGOCLIENT_INCLUDE_DIR 
 	mongo/config.h
     HINTS ${CMAKE_CURRENT_LIST_DIR}/include
-)
-
-find_library(BOOST_SYSTEM
-    boost_system
-)
-
-find_library(BOOST_THREAD
-    boost_thread
-)
-
-find_library(BOOST_REGEX
-    boost_regex
-)
-
-find_path(BOOST_INCLUDE_DIR 
-    boost/version.hpp
 )
