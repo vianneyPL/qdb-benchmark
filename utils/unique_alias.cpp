2279d4833447b5763c48a9c1be403d179e50ccac
Benoit Blanchon <benoit@quasardb.net>
2016-01-13 17:03:19 +0100

    Use proper base 64 encoding for the watermark

--- utils/unique_alias.cpp
+++ utils/unique_alias.cpp
@@ -6,15 +6,15 @@
 
 static std::atomic<int> __counter(0);
 
+static const char __base64[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
 utils::unique_alias::unique_alias()
 {
     std::ostringstream s;
 
     s << "benchmarks-"
-      << std::chrono::duration_cast<std::chrono::seconds>(
-             std::chrono::steady_clock::now().time_since_epoch())
-             .count()
-      << "-" << __counter++ << "-000000";
+      << std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now().time_since_epoch()).count()
+      << "-" << __counter++ << "-AAAAAA"; // reserve 6 chars for the watermark
 
     _string = s.str();
 }
@@ -24,7 +24,7 @@ void utils::unique_alias::set_watermark(unsigned long iteration)
     size_t index = _string.size();
     for (int digit = 0; digit < 6; digit++)
     {
-        _string[--index] = '0' + (iteration & 63);
+        _string[--index] = __base64[iteration & 63];
         iteration /= 64;
     }
 }
